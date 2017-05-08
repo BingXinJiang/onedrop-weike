0c6c7fccb2a8c47ffe9d9eb821881dd833fc655f
BinXinJiang <642823656@qq.com>
2017-05-08 14:00:17 +0800

    环境更改

--- client/public/js/const/onedrop.js
+++ client/public/js/const/onedrop.js
@@ -5,12 +5,12 @@
 module.exports = {
     // base_ip:'http://localhost:3000/weixin',
     // res_ip:'http://localhost:3000',
-    // base_ip:'http://192.168.1.181:3000/weixin',
-    // res_ip:'http://192.168.1.181:3000',
+    base_ip:'http://192.168.1.181:3000/weixin',
+    res_ip:'http://192.168.1.181:3000',
     // base_ip:'http://192.168.2.100:3000/weixin',
     // res_ip:'http://192.168.2.100:3000',
-    base_ip:'http://www.cvwisdom.com/weixin',
-    res_ip:'http://www.cvwisdom.com',
+    // base_ip:'http://www.cvwisdom.com/weixin',
+    // res_ip:'http://www.cvwisdom.com',
     
     appId:'wxcb05ae4237186327'
 }
