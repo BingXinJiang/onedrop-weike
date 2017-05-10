1f48bdac3e91f11d10c7b5c0f1b0670fdd9d9653
BinXinJiang <642823656@qq.com>
2017-05-10 09:37:12 +0800

    UI框架布局

--- client/public/js/view/home/main.js
+++ client/public/js/view/home/main.js
@@ -3,32 +3,82 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link } from 'react-router';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 
 import Onedrop from '../drop/Onedrop';
 import Evaluation from '../evalution/Evaluation';
 import Question from '../question/Question';
 import Course from '../main/index';
 
+var ScreenW = document.body.clientWidth;
+
 class Main extends React.Component{
     constructor(props){
         super(props);
     }
     render(){
-        return <div>
+        var tabBtnW = ScreenW/4 + 'px';
+        console.log('屏幕宽度:', ScreenW);
+        return (
+            <div>
+                <Route exact path='/weixin/main/' component={Onedrop}/>
+                <Route path='/weixin/main/courses' component={Course}/>
+                <Route path='/weixin/main/question' component={Question}/>
+                <Route path='/weixin/main/evaluation' component={Evaluation}/>
 
-        </div>
+                <ul style={{}}>
+                    {
+                        [
+                            {
+
+                            },
+                            {
+                                
+                            }
+                        ]
+                    }
+                    <li style={{float:'left',width:tabBtnW,
+                    textAlign:'center',listStyleType:'none'
+                    }}>
+                        <Link style={{backgroundColor:'red'
+
+                        }} to="/weixin/main/">一滴</Link>
+                    </li>
+                    <li style={{float:'left',width:tabBtnW,
+                    textAlign:'center',listStyleType:'none'
+                    }}>
+                        <Link style={{
+
+                        }} to="/weixin/main/courses">课程</Link>
+                    </li>
+                    <li style={{float:'left',width:tabBtnW,
+                    textAlign:'center',listStyleType:'none'
+                    }}>
+                        <Link style={{
+
+                        }} to="/weixin/main/question">问答</Link>
+                    </li>
+                    <li style={{float:'left',width:tabBtnW,
+                    textAlign:'center',listStyleType:'none'
+                    }}>
+                        <Link style={{
+
+                        }} to="/weixin/main/evaluation">测评</Link>
+                    </li>
+                </ul>
+
+            </div>
+        )
     }
 }
 
 ReactDOM.render(
     <Router>
-        <Route path='/' component={Main}>
-            <Route path='/onedrop' component={Onedrop}/>
-            <Route path='/courses' component={Course}/>
-            <Route path='/question' component={Question}/>
-            <Route path='/evaluation' component={Evaluation}/>
-        </Route>
+        <Main/>
     </Router>,
     document.getElementById('main-container')
 )
