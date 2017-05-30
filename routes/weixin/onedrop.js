899eb1d677c986e347c80fd3e2307c50db7ebcf7
BinXinJiang <642823656@qq.com>
2017-05-30 21:19:06 +0800

    后端

--- routes/weixin/onedrop.js
+++ routes/weixin/onedrop.js
@@ -50,19 +50,12 @@ router.get('/active', function (req, res, next) {
     })
 })
 /**
- * 获取首页一滴每天推送的课程
+ * 获取首页一滴每一天推送的课程
  * 参数：user_id
  * */
 router.post('/section', function (req, res, next) {
     var user_id = req.body.user_id;
-    console.log('user_id:', user_id);
-    // var query_sql = "select course_id,section_id,course_author,section_name,section_des,open_date from course_section where open_date<Now() " +
-    //     "and open_date != 0";
-
-    // var query_sql = "select year(open_date)year,month(open_date)month,day(open_date)day," +
-    //     "course_id,section_id,course_author,section_name,section_des,open_date from course_section " +
-    //     "where open_date<Now() and open_date != 0 " +
-    //     "group by year,month,day";
+    // console.log('user_id:', user_id);
 
     var query_sql = "select year(open_date)year,month(open_date)month,day(open_date)day," +
         "course_id,section_id,course_author,section_name,section_des,open_date from course_section " +
@@ -133,6 +126,83 @@ router.post('/section/detail', function (req, res, next) {
             }
         }
     })
+})
+
+/**
+ * 获取当天推送的某一小节的具体信息
+ * */
+router.post('/every_day', function (req, res, next) {
+    var user_id = req.body.user_id;
+
+    //判断该user_id是否是会员,是否可以免费听取课程
+
+    //判断是否当天已经打卡
+    async.parallel([
+        function (callback) {
+            var judge_sql = "select punch_time from punch_card where user_id = " +
+                "'"+user_id+"' and punch_time>=curdate() and punch_time<date_sub(curdate(),interval -1 day)";
+            query(judge_sql, function (qerr, valls, fields) {
+                if(qerr){
+                    responseDataErr(res);
+                }else{
+                    if(valls.length>0){
+                        //已经打卡
+                        callback(null, true)
+                    }else{
+                        //还没有打卡
+                        callback(null, false)
+                    }
+                }
+            })
+        },
+        function (callback) {
+            var query_sql = "select a.course_id,a.course_title,a.author_id,a.section_voice,a.section_des," +
+                "b.teacher_position from " +
+                "(select * from course_section where Now()>=open_date and Now()<date_sub(open_date,interval -1 day))as a " +
+                "left join (select * from teacher)as b " +
+                "on a.author_id = b.teacher_id";
+            query(query_sql, function (qerr, valls, fields) {
+                if(qerr){
+                    responseDataErr(res);
+                }else if(valls.length != 1){
+                    responseDataErr(res);
+                }else{
+                    callback(null, valls[0])
+                }
+            })
+        },
+        function (callback) {
+            var user_sql = "select user_id,nickname,headimgurl,fraction from user " +
+                "where user_id='"+user_id+"'";
+            query(user_sql, function (qerr, valls, fields) {
+                if(qerr){
+                    responseDataErr(res);
+                }else{
+                    if(valls.length>0){
+                        callback(null, valls[0]);
+                    }else{
+                        responseDataErr(res);
+                    }
+                }
+            })
+        }
+    ], function (err, results) {
+        if(err){
+            responseDataErr(res);
+        }else{
+            var data = {
+                isPanchCard:results[0],
+                course:results[1],
+                user:results[2]
+            };
+            var response = {
+                status:1,
+                data:data
+            }
+            res.json(response);
+        }
+    })
+
 })
 
 module.exports = router;
--- routes/weixin/punchcard.js
+++ routes/weixin/punchcard.js
@@ -15,52 +15,75 @@ var parseString = require('xml2js').parseString;
 
 var async = require('async');
 
-/**
- * 获取每天用户所在团队打卡最早的三个用户
- * 参数：user_id
- * */
-router.post('/three/bird', function (req, res, next) {
-    var user_id = req.body.user_id;
-    
-})
-
-/**
- * 获取个人积分情况，排名情况，所在团队积分情况，所在团队的排名情况
- * */
-
-/**
- * 获取当天推荐的座右铭的情况
- * */
-
+function responseDataErr(res) {
+    var response = {
+        status:0,
+        data:{
+            msg:'数据库执行错误'
+        }
+    }
+    res.json(response);
+}
 /**
  * 用户打卡
+ * 参数: user_id motto  section_id
  * */
+router.post('/punch_card', function (req, res, next) {
+    var user_id = req.body.user_id;
+    var motto = req.body.motto;
+    var section_id = req.body.section_id;
 
-/**
- * 获取用户排行榜
- * */
+    async.waterfall([
+        function (callback) {
+            var query_sql = "select datediff(Now(),select open_date from course_section" +
+                " where section_id="+section_id+")";
+            query(query_sql, function (qerr, valls, fields) {
+                if(qerr){
+                    responseDataErr(res);
+                }else{
+                    if(valls.length>0){
+                        var datediffNum = Number(valls[0]);
+                        if(datediffNum == 0){
+                            callback(null, 2);
+                        }else if(datediffNum == 1){
+                            callback(null, 1);
+                        }else{
+                            callback(null, 0);
+                        }
+                    }else{
+                        responseDataErr(res);
+                    }
+                }
+            })
+        },
+        function (fraction,callback) {
+            var punch_id = (new Date()).getTime()+ "" + Math.random()*100000;
+            var insert_sql = "insert into punch_card(punch_id,user_id,punch_time,motto,punch_fraction,section_id) " +
+                "values('"+punch_id+"','"+user_id+"',Now(),'"+motto+"',"+fraction+","+section_id+")";
+            query(insert_sql, function (qerr, valls, fields) {
+                if(qerr){
+                    responseDataErr(res);
+                }else{
+                    callback(null, 'ok');
+                }
+            })
+        }
+    ], function (err, results) {
+        if(err){
+            responseDataErr(res);
+        }else{
+            var response = {
+                status:1,
+                data:{
+                    msg:'打卡成功!'
+                }
+            }
+            res.json(response);
+        }
+    })
+})
 
-/**
- * 获取团队排行榜
- * */
-
-/**
- * 按时间倒序获取所有人的打卡状态
- * */
-
-/**
- * 获取往日一滴内容
- * */
-
-/**
- * 获取我自己的打卡历程
- * */
-
-/**
- *
- * */
-
-
+module.exports = router;
 
 
 
