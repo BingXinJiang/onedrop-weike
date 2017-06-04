33ebedaf6b359492cd5211f68bb0f97caad0aeed
BinXinJiang <642823656@qq.com>
2017-06-04 11:35:43 +0800

    首页banner图更换

--- client/public/js/const/onedrop.js
+++ client/public/js/const/onedrop.js
@@ -11,8 +11,8 @@ module.exports = {
     // base_url:'http://192.168.1.181:3000',
     // res_ip:'http://192.168.1.181:3000',
 
-    // base_ip:'http://192.168.2.104:3001/weixin',
-    // base_url:'http://192.168.2.104:3001',
+    base_ip:'http://192.168.2.104:3001/weixin',
+    base_url:'http://192.168.2.104:3001',
     // res_ip:'http://192.168.2.104:3001',
 
     // base_ip:'http://192.168.1.193:3000/weixin',
@@ -24,8 +24,8 @@ module.exports = {
     // base_url:'http://www.cvwisdom.com',
     // res_ip:'http://www.cvwisdom.com',
 
-    base_ip:'http://drop.cvwisdom.com/weixin',
-    base_url:'http://drop.cvwisdom.com',
+    // base_ip:'http://drop.cvwisdom.com/weixin',
+    // base_url:'http://drop.cvwisdom.com',
     res_ip:'http://drop.cvwisdom.com',
 
     // base_ip:'http://119.23.24.122:3001/weixin',
