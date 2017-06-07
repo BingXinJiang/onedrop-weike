7715b7a06cedb43259b29e2c4ef7ee74937bc968
蒋嵩 <642823656@qq.com>
2017-06-07 18:47:45 +0800

    无更改

--- client/public/js/const/onedrop.js
+++ client/public/js/const/onedrop.js
@@ -15,8 +15,8 @@ module.exports = {
     // base_url:'http://192.168.2.104:3001',
     // res_ip:'http://192.168.2.104:3001',
 
-    // base_ip:'http://192.168.1.29:3001/weixin',
-    // base_url:'http://192.168.1.29:3001',
+    base_ip:'http://192.168.1.29:3001/weixin',
+    base_url:'http://192.168.1.29:3001',
     // res_ip:'http://192.168.1.29:3001',
 
     // base_ip:'http://192.168.1.193:3000/weixin',
@@ -28,8 +28,8 @@ module.exports = {
     // base_url:'http://www.cvwisdom.com',
     // res_ip:'http://www.cvwisdom.com',
 
-    base_ip:'http://drop.cvwisdom.com/weixin',
-    base_url:'http://drop.cvwisdom.com',
+    // base_ip:'http://drop.cvwisdom.com/weixin',
+    // base_url:'http://drop.cvwisdom.com',
     res_ip:'http://drop.cvwisdom.com',
 
     // base_ip:'http://119.23.24.122:3001/weixin',
