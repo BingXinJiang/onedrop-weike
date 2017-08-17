d337c6e415343b6eaf7c4c14dd3c50801e3008ba
蒋嵩 <642823656@qq.com>
2017-08-17 18:10:02 +0800

    Push汉字编码

--- routes/tool/Push.js
+++ routes/tool/Push.js
@@ -124,7 +124,7 @@ module.exports = {
                                                 "msgtype":"text",
                                                 "text":
                                                     {
-                                                        "content":"&#x903B;&#x8F91;&#x601D;&#x7EF4;&#x7528;&#x5C0F;&#x7EC4;&#x5236;&#x8FDB;&#x884C;&#x7BA1;&#x7406;&#xFF01;&#xFF01;"
+                                                        "content":"\u7edd\u5730\u9022\u751f\u5ba2\u6237\u7aef\u5206\u4e09\u5757\u8303\u8fd8\u662f\u6b3e\u5230\u53d1\u8d27\u6848\u4ef6\u56de\u8bbf\u770b\u5bf9\u65b9\u65f6\u95f4\u6ed1\u52a8\u4ed8\u6b3e\u540e"
                                                     }
                                             });
                 console.log('推送信息的主体：',postData);
